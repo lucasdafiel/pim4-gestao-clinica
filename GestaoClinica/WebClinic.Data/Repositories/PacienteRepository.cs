70e4c25e157dba35ff28e8700a29c7192be7c66b
Lucas Micael <lucasmicaellds@gmail.com>
2025-09-28 13:50:04 -0300

    Correção de referências

--- GestaoClinica/WebClinic.Data/Repositories/PacienteRepository.cs
+++ GestaoClinica/WebClinic.Data/Repositories/PacienteRepository.cs
@@ -7,49 +7,52 @@ using WebClinic.Data.Context;
 using WebClinic.Core.Interfaces;
 using WebClinic.Core.Models;
 
-public class PacienteRepository : IPacienteRepository
+namespace WebClinic.Data.Repositories
 {
-    private readonly WebClinicContext _context;
-
-    public PacienteRepository(WebClinicContext context)
+    public class PacienteRepository : IPacienteRepository
     {
-        _context = context;
-    }
+        private readonly WebClinicContext _context;
 
-    public void Adicionar(Paciente paciente)
-    {
-        _context.Pacientes.Add(paciente);
-        _context.SaveChanges(); // Salva as mudanças no banco de dados
-    }
-
-    public Paciente? ObterPorId(int id)
-    {
-        return _context.Pacientes.Find(id);
-    }
-
-    public Paciente? ObterPorCPF(string cpf)
-    {
-        return _context.Pacientes.FirstOrDefault(p => p.CPF == cpf);
-    }
-
-    public List<Paciente> ListarTodos()
-    {
-        return _context.Pacientes.ToList();
-    }
-
-    public void Atualizar(Paciente paciente)
-    {
-        _context.Pacientes.Update(paciente);
-        _context.SaveChanges();
-    }
-
-    public void Excluir(int id)
-    {
-        var paciente = ObterPorId(id);
-        if (paciente is not null)
+        public PacienteRepository(WebClinicContext context)
         {
-            _context.Pacientes.Remove(paciente);
+            _context = context;
+        }
+
+        public void Adicionar(Paciente paciente)
+        {
+            _context.Pacientes.Add(paciente);
+            _context.SaveChanges(); // Salva as mudanças no banco de dados
+        }
+
+        public Paciente? ObterPorId(int id)
+        {
+            return _context.Pacientes.Find(id);
+        }
+
+        public Paciente? ObterPorCPF(string cpf)
+        {
+            return _context.Pacientes.FirstOrDefault(p => p.CPF == cpf);
+        }
+
+        public List<Paciente> ListarTodos()
+        {
+            return _context.Pacientes.ToList();
+        }
+
+        public void Atualizar(Paciente paciente)
+        {
+            _context.Pacientes.Update(paciente);
             _context.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            var paciente = ObterPorId(id);
+            if (paciente is not null)
+            {
+                _context.Pacientes.Remove(paciente);
+                _context.SaveChanges();
+            }
         }
     }
 }
