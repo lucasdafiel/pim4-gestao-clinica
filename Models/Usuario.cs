310a34ea8dc5cab9fe929bf9ad5e8ba228789efa
Lucas Micael <lucasmicaellds@gmail.com>
2025-10-16 21:41:23 -0300

    Delete Models directory

--- Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-// Local: /Models/Usuario.cs
-namespace WebClinicSystem.Models;
-
-public class Usuario
-{
-    public int Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string Perfil { get; set; } = string.Empty; // Ex: Administrador, Recepcionista
-}
